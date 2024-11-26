e4840e0189a38bb25d481c0439ff3345832a4397
Hau Hsu <hau.hsu@sifive.com>
2024-11-26 17:07:22 +0800

    Ch8.5
    Signed-off-by: Hau Hsu <hau.hsu@sifive.com>

--- lox/src/com/craftinginterpreters/lox/Interpreter.java
+++ lox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -101,6 +101,20 @@ class Interpreter implements Expr.Visitor<Object>,
 		stmt.accept(this);
 	}
 
+	void executeBlock(List<Stmt> statements,
+			Environment environment) {
+		Environment previous = this.environment;
+		try {
+			this.environment = environment;
+
+			for (Stmt statement : statements) {
+				execute(statement);
+			}
+		} finally {
+			this.environment = previous;
+		}
+	}
+
 	@Override
 	public Void visitExpressionStmt(Stmt.Expression stmt) {
 		evaluate(stmt.expression);
@@ -124,6 +138,19 @@ class Interpreter implements Expr.Visitor<Object>,
 	  environment.define(stmt.name.lexeme, value);
 	  return null;
 	}
+
+	 @Override
+	 public Object visitAssignExpr(Expr.Assign expr) {
+		 Object value = evaluate(expr.value);
+		 environment.assign(expr.name, value);
+		 return value;
+	 }
+
+	 @Override
+	 public Void visitBlockStmt(Stmt.Block stmt) {
+		 executeBlock(stmt.statements, new Environment(environment));
+		 return null;
+	 }
 
 	private boolean isTruthy(Object object) {
 		if (object == null) return false;
